43d47cd409e6079fb56dba4af30b1321ad0d7c0b
Murtaza Ali <murtazaalimashadi1999@gmail.com>
2022-03-24 11:03:14 +0500

    minor change

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const MONGOURI = require("./Keys/keys").MONGOURI;
+const PORT = 5533;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({})); // for parsing json object, on POST/PUT request
 
-const admin = require("./routes/admin");
-const shop = require("./routes/shop");
-const auth = require("./routes/auth");
-
-app.use([admin, shop, auth]);
-
-const PORT = 4444;
-
-app.listen(PORT, async () => {
-  console.log(`Server is running on port #${PORT}`);
-});
+//app.use([]);
 
 app.get("/", async (req, res) => {
-  res.send(`
-    <h1>Welcome Page</h1>`);
+  res.send(`<h1>Welcome</h1>`);
 });
 
-app.post("/testRoute", async (req, res) => {
-  console.log(req.body);
+mongoose.connect(MONGOURI, () => {
+  console.log("MongoDB connection made successfully");
+  app.listen(PORT, async () => {
+    console.log(`Server is running on port #${PORT}`);
+  });
 });
